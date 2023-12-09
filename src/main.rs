ad89256e88802ea66420836ac28b8bfe6b0a0278
Ben Van Sleen <benvansleen@gmail.com>
2023-12-09 17:23:57 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+
+
+fn read_input(day: &str, part: &str) -> Vec<String> {
+    if atty::is(atty::Stream::Stdin) {
+        let filename = format!("inputs/day{day}/part{part}");
+        std::fs::read_to_string(filename)
+            .expect("could not read input file")
+            .lines()
+            .map(|line| line.to_string())
+            .collect()
+    }
+    else {
+        std::io::stdin()
+            .lines()
+            .filter_map(|line| line.ok())
+            .collect()
+    }
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let day = args.get(1).expect("missing day argument");
+    let part = args.get(2).expect("missing part argument");
+    let input = read_input(day, part);
+
+    let output = match day.as_str() {
+        "1" => {
+            use advent_of_code_2023::day1;
+            match part.as_str() {
+                "1" => day1::part1(&input),
+                "2" => day1::part2(&input),
+                _ => panic!("invalid part"),
+            }
+        },
+        _ => panic!("unknown day"),
+    };
+
+    println!("{output}");
+}
