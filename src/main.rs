10ea6959eabc859ac30c900bae3560ed390c15db
Ben Van Sleen <benvansleen@gmail.com>
2023-12-09 23:36:16 -0500

    Fetch input file from AoC if not present on local filesystem

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
 use std::env;
 
-fn read_input(day: &str, part: &str) -> Vec<String> {
+fn fetch_input(day: &str) -> String {
+    println!("Fetching input for day {}...", day);
+    if !std::path::Path::new(".session").exists() {
+        panic!("No session file found");
+    }
+
+    let session = std::fs::read_to_string(".session")
+        .expect("could not read session file");
+    let url = format!("https://adventofcode.com/2023/day/{day}/input",);
+
+    ureq::get(&url)
+        .set("Cookie", session.trim())
+        .call()
+        .expect("could not fetch input")
+        .into_string()
+        .expect("could not parse input")
+}
+
+fn read_input(day: &str) -> Vec<String> {
     if atty::is(atty::Stream::Stdin) {
-        let filename = format!("inputs/day{day}/part{part}");
-        std::fs::read_to_string(filename)
-            .expect("could not read input file")
-            .lines()
+        if !std::path::Path::new("inputs").exists() {
+            std::fs::create_dir("inputs")
+                .expect("Failed to create inputs directory");
+        }
+
+        let filename = format!("inputs/day{day}");
+
+        let contents = match std::fs::read_to_string(&filename) {
+            Ok(contents) => contents,
+            Err(_) => {
+                let contents = fetch_input(day);
+                std::fs::write(&filename, &contents)
+                    .expect("Failed to write input file");
+                contents
+            }
+        };
+
+        contents
+            .trim()
+            .split('\n')
             .map(|line| line.to_string())
             .collect()
     } else {
-        std::io::stdin()
-            .lines()
-            .map_while(Result::ok)
-            .collect()
+        std::io::stdin().lines().map_while(Result::ok).collect()
     }
 }
 
@@ -20,7 +51,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let day = args.get(1).expect("missing day argument");
     let part = args.get(2).expect("missing part argument");
-    let input = read_input(day, part);
+    let input = read_input(day);
 
     let output = match day.as_str() {
         "1" => {
