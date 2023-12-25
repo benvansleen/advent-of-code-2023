95ce76f01ede5484d60fc466bfe3511a47726d29
Ben Van Sleen <benvansleen@gmail.com>
2023-12-24 19:21:37 -0500

    Passing part 1

---
+++ src/day4.rs
@@ -0,0 +1,76 @@
+use std::collections::HashSet;
+
+#[derive(Debug)]
+struct Card {
+    _id: usize,
+    winners: HashSet<u32>,
+    values: HashSet<u32>,
+}
+
+impl Card {
+    fn from(s: &str) -> Self {
+        let split_on_colon: Vec<&str> = s.split(":").collect();
+        let id = split_on_colon[0]
+            .split(" ")
+            .collect::<Vec<_>>()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+
+        let to_set = |s: &str| {
+            s.split(" ")
+                .filter(|s| !s.is_empty())
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<HashSet<_>>()
+        };
+        let game: Vec<&str> = split_on_colon[1].split("|").collect();
+
+        Self {
+            _id: id,
+            winners: to_set(game[0]),
+            values: to_set(game[1]),
+        }
+    }
+
+    fn value(&self) -> u32 {
+        let n_winners = self.winners.intersection(&self.values).count();
+        if n_winners == 0 {
+            return 0;
+        }
+
+        2_u32.pow(n_winners as u32 - 1)
+    }
+}
+
+pub fn part1(input: &[String]) -> u32 {
+    input
+        .iter()
+        .map(|s| Card::from(s))
+        .inspect(|c| log::debug!("{:?}", c))
+        .map(|c| c.value())
+        .inspect(|c| log::debug!("{:?}", c))
+        .sum()
+}
+
+pub fn part2(input: &[String]) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let input = [
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ]
+        .map(|s| s.to_string());
+
+        assert_eq!(super::part1(&input), 13);
+    }
+}
